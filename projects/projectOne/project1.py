f457e5accca127a256784bf200beba86fd1bbe5f
Justin Peck <jdpeck90@gmail.com>
2019-03-09 18:54:05 -0500

    finished first project, started uploading modules for second project

--- projects/projectOne/project1.py
+++ projects/projectOne/project1.py
@@ -7,19 +7,33 @@ data = json.load(open("data.json"))
 
 
 def translate(w):
-    print("before", w)
     w = w.lower()
-    print("after", w)
     if w in data:
         return data[w]
     elif len(get_close_matches(w, data.keys())) > 0:
-        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
+        responseToWordRatio = input(
+            "Did you mean %s instead? Enter Y if yes, or N if no:  " % get_close_matches(w, data.keys())[0])
+        if responseToWordRatio == "Y":
+            x = get_close_matches(w, data.keys())[0].capitalize()
+            y = " is "
+            z = data[get_close_matches(w, data.keys())[0]][0]
+            zz = "%s%s%s" % (x, y, z)
+            return zz
+        elif responseToWordRatio == "N":
+            return "The word doesn't exist. Please double check it"
+        else:
+            return "We didn't understand your entry"
     else:
-
         return "The word doesn't exist. Please double check input."
 
 
 word = input("enter word: ")
 
 
-print(translate(word))
+output = translate(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
