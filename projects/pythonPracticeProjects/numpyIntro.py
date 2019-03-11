5f41305c6273675a1edffe1bf323aba2597b20bf
Justin Peck <jdpeck90@gmail.com>
2019-03-10 22:13:40 -0400

    start 2nd project with folium

---
+++ projects/pythonPracticeProjects/numpyIntro.py
@@ -0,0 +1,9 @@
+import numpy
+import cv2
+
+pictoarray = cv2.imread('smallgray.png')
+print(pictoarray)
+
+n = numpy.arange(27)
+
+print('i am printing the resuts from n ------->                   ', n)
