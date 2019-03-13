e102532282cca68a06cbacb14c582f298c934b0d
Justin Peck <jdpeck90@gmail.com>
2019-03-13 14:44:35 -0400

    finished up folium, volcanoe map

--- projects/projectTwo/map1.py
+++ projects/projectTwo/map1.py
@@ -22,19 +22,25 @@ def color_producer(elevation):
 map = folium.Map(location=[39.94, -75.14],
                  zoom_start=18)
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.CircleMarker(location=[lt, ln],
-                                     radius=6,
-                                     popup=str(el) + " m",
-                                     fill_color=color_producer(el),
-                                     color='grey',
-                                     fill_opacity=0.7
-                                     ))
+    fgv.add_child(folium.CircleMarker(location=[lt, ln],
+                                      radius=6,
+                                      popup=str(el) + " m",
+                                      fill_color=color_producer(el),
+                                      color='grey',
+                                      fill_opacity=0.7
+                                      ))
 
-fg.add_child(folium.GeoJson(
-    data=(open("world.json", "r", encoding="utf-8-sig").read())))
+fgp = folium.FeatureGroup(name="Population")
+
+
+fgp.add_child(folium.GeoJson(
+    data=(open("world.json", "r", encoding="utf-8-sig").read()),
+    style_function=lambda x: {'fillColor': "green" if x['properties']['POP2005'] < 10000000
+                              else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}
+))
 
 # popupText = ["Hi I am a Marker", "Second popup text"]
 # iconColor = ["green", "blue"]
@@ -46,8 +52,12 @@ fg.add_child(folium.GeoJson(
 
 
 # print('from map1.py printing the coordinates ---> ', coordinates)
-map.add_child(fg)
 
+
+map.add_child(fgv)
+map.add_child(fgp)
+
+map.add_child(folium.LayerControl())
 filename = 'Map1.html'
 full_filename_path = os.path.realpath(filename)
 
