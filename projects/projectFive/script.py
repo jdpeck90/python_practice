41edc7d5ba1e5a43e45f31253e91be0816caedd0
Justin Peck <jdpeck90@gmail.com>
2019-03-15 10:22:14 -0400

    starting with tkinter

--- projects/projectFive/script.py
+++ projects/projectFive/script.py
@@ -2,14 +2,21 @@ from tkinter import *
 
 window = Tk()
 
-b1 = Button(window, text="Execute")
+
+def km_to_miles():
+    miles = float(el_value.get())*1.6
+    tl.insert(END, miles)
+
+
+b1 = Button(window, text="Execute", command=km_to_miles)
 b1.grid(row=0, column=0)
 
 
-el = Entry(window)
+el_value = StringVar()
+el = Entry(window, textvariable=el_value)
 el.grid(row=0, column=1)
 
 
-tl = Text(window, height=20)
+tl = Text(window, height=1, width=20)
 tl.grid(row=0, column=2)
 window.mainloop()
